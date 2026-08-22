da73f2815a730e895fc8b7cc9518e96812e13632
raji0704 <akularajeswari120@gmail.com>
2026-08-22 22:07:23 +0530

    Added Level 1 Problem 2

---
+++ src/session_1/Level1/Problem2.java
@@ -0,0 +1,29 @@
+package session_1.Level1;
+import java.util.Scanner;
+
+public class Problem2 {
+
+    void classifyNumber(int number) {
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        Problem2 obj = new Problem2();
+        obj.classifyNumber(number);
+
+        sc.close();
+    }
+}
