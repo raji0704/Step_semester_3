85a6387f1f9c6cb4c8d5e9ec785e28a064f3012b
raji0704 <akularajeswari120@gmail.com>
2026-08-22 22:01:24 +0530

    Added Level 1 Problem 1

---
+++ src/session_1/Level1/Problem1.java
@@ -0,0 +1,27 @@
+package session_1.Level1;
+ import java.util.Scanner;
+
+public class Problem1 {
+
+    void checkVotingEligibility(int age) {
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        Problem1 obj = new Problem1();
+        obj.checkVotingEligibility(age);
+
+        sc.close();
+    }
+}
