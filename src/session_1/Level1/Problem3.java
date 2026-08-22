dafbfdc59f14d11669c19f2cd1d1a5963126f538
raji0704 <akularajeswari120@gmail.com>
2026-08-22 22:10:19 +0530

    Added Level 1 Problem 3

---
+++ src/session_1/Level1/Problem3.java
@@ -0,0 +1,25 @@
+package session_1.Level1;
+import java.util.Scanner;
+
+public class Problem3 {
+
+    void printNumbers(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        Problem3 obj = new Problem3();
+        obj.printNumbers(n);
+
+        sc.close();
+    }
+}
